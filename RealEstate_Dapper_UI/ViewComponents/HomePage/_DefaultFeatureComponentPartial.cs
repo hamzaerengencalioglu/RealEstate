875cf1e9ff19b2bea6b7b910315ef4013f20e933
hamzaerengencalioglu <hamzaerengencalioglu@gmail.com>
2024-07-15 16:32:10 +0300

    RealEstate_Dapper_UI updated

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultFeatureComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultFeatureComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
